c0f7dfeb904827c522f2b88edc06ac62c592978f
ju <carlvall@gmail.com>
2019-10-01 18:09:26 -0500

    Lots of translations added and some minor fixes.

--- src/Admin/EmailSubscriptionAdmin.php
+++ src/Admin/EmailSubscriptionAdmin.php
@@ -7,26 +7,35 @@ use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Sonata\AdminBundle\Form\Type\ModelListType;
 
 final class EmailSubscriptionAdmin extends AbstractAdmin
 {
     protected function configureFormFields(FormMapper $formMapper)
     {
-        $formMapper->add('isActive', CheckboxType::class);
-        $formMapper->add('periodicity', TextType::class);
-        $formMapper->add('daysAhead', TextType::class);
+        $formMapper->add('isActive', CheckboxType::class,[
+            'label' => 'email_subscription.label.is_active'
+        ]);
+        $formMapper->add('periodicity', ChoiceType::class, [
+            'choices'  => ["Diariamente"=> '1',
+                           'Semanal'=> '7',
+                           'Quinzenal'=>'15'] ,
+            'label' => 'email_subscription.label.periodicity',
+            'expanded' => false,
+            'multiple' => false,
+            'required' => true,
+        ]);
+        $formMapper->add('daysAhead', ChoiceType::class, [
+            'choices'  => ["1"=> 1,
+                           '2'=> 2,
+                           '3'=> 3] ,
+            'label' => 'email_subscription.label.days_ahead',
+            'expanded' => false,
+            'multiple' => false,
+            'required' => true,
+        ]);
 
-    }
-    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
-    {
-        $datagridMapper->add('periodicity');
-    }
-    protected function configureListFields(ListMapper $listMapper)
-    {
-        $listMapper->addIdentifier('periodicity');
-     
     }
 
 }
--- src/Admin/LiturgyAdmin.php
+++ src/Admin/LiturgyAdmin.php
@@ -6,46 +6,121 @@ use Sonata\AdminBundle\Admin\AbstractAdmin;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
 use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Sonata\AdminBundle\Show\ShowMapper;
 
-
+/**
+ * Sonata Admin for the Liturgy.
+ *
+ *
+ */
 final class LiturgyAdmin extends AbstractAdmin
 {
     
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->with('General', ['class' => 'col-md-7'])->end()
+            ->with(
+                'General',
+                ['class' => 'col-md-7', 'label' => 'liturgy.title.general']
+            )
+            ->end()
             ->with('Special', ['class' => 'col-md-5'])->end()
-            ->with('Text',['class' => 'col-md-12'])->end()
+            ->with('Text', ['class' => 'col-md-7'])->end()
             ;
 
         $formMapper
             ->with('General')
-                ->add('date')
-                ->add('liturgyDay')
-            ->add('description',  TextType::class)
-                ->add('color')
-            ->add('yearType')
+            ->add('Summary', TextType::class, ['label' => 'liturgy.label.summary',])
+            ->add('date', DateType::class, ['label' => 'liturgy.label.date'])
+            ->add('liturgyDay', TextType::class, ['label' => 'liturgy.label.liturgy_day',])
+            ->add(
+                'description',
+                TextType::class,
+                ['label' => 'liturgy.label.description',]
+            )
+            ->add(
+                'color',
+                ChoiceType::class,
+                ['choices'  => [
+                          'Azul' => 'Azul',
+                          'Branco' => 'Branco',
+                          'Dourado' => 'Dourado',
+                          'Preto' => 'Preto',
+                          'Rosa' => 'Rosa',
+                          'Roxo' => 'Roxo',
+                          'Vermelho' => 'Vermelho',
+                         'Verde' => 'Verde',
+                      ],
+                 'expanded' => false,
+                 'multiple' => false,
+                 'required' => true,
+                 'label' => 'liturgy.label.color',
+                ]
+            )
+            ->add(
+                'yearType',
+                ChoiceType::class,
+                ['choices'  => [
+                          'A' => 'A',
+                          'B' => 'B',
+                          'C' => 'C',
+                      ],
+                 'expanded' => false,
+                 'multiple' => false,
+                 'required' => true,
+                 'label' => 'liturgy.label.year_type',
+                ]
+            )
             ->end()
-            ->with('Special')
-                ->add('isSolemnity')
-                ->add('isSolemnityVFC')
-                ->add('isCelebration')
-                ->add('isCelebrationVFC')
-                ->add('isMemorial')
-                ->add('isMemorialVFC')
-                ->add('isMemorialFree')
+            ->with('Special', ['label' => 'liturgy.title.special'])
+            ->add(
+                'isSolemnity',
+                null,
+                ['label' => 'liturgy.label.is_solemnity']
+            )
+            ->add(
+                'isSolemnityVFC',
+                null,
+                ['label' => 'liturgy.label.is_solemnity_vfc']
+            )
+            ->add(
+                'isCelebration',
+                null,
+                ['label' => 'liturgy.label.is_celebration']
+            )
+            ->add(
+                'isCelebrationVFC',
+                null,
+                ['label' => 'liturgy.label.is_celebration_vfc']
+            )
+            ->add('isMemorial', null, ['label' => 'liturgy.label.is_memorial'])
+            ->add(
+                'isMemorialVFC',
+                null,
+                ['label' => 'liturgy.label.is_memorial_vfc']
+            )
+            ->add(
+                'isMemorialFree',
+                null,
+                ['label' => 'liturgy.label.is_memorial_free']
+            )
             ->end()
-            ->with('Text')
-            ->add('alleluiaReference',  TextType::class)
-            ->add('alleluiaVerse',  TextType::class)
-            ->add('summary',  TextType::class)
+            ->with('Text', ['label' => 'liturgy.title.text'])
+            ->add(
+                'alleluiaReference',
+                TextType::class,
+                ['label' => 'liturgy.label.alleluia_reference']
+            )
+            ->add(
+                'alleluiaVerse',
+                TextType::class,
+                ['label' => 'liturgy.label.alleluia_verse']
+            )
             ->end()
             ;
-
     }
 
     protected function configureDatagridFilters(DatagridMapper $datagridMapper)
@@ -66,56 +141,98 @@ final class LiturgyAdmin extends AbstractAdmin
 
     protected function configureListFields(ListMapper $listMapper)
     {
-        $listMapper->addIdentifier('date');
-        $listMapper->addIdentifier('liturgyDay');
-        $listMapper->addIdentifier('description');
-        $listMapper->addIdentifier('color');
-        $listMapper->addIdentifier('isSolemnity');
-        $listMapper->addIdentifier('isSolemnityVFC');
-        $listMapper->addIdentifier('isCelebration');
-        $listMapper->addIdentifier('isCelebrationVFC');
-        $listMapper->addIdentifier('isMemorial');
-        $listMapper->addIdentifier('isMemorialVFC');
-        $listMapper->addIdentifier('isMemorialFree');
-        $listMapper->addIdentifier('yearType');
-        $listMapper->addIdentifier('alleluiaReference');
-        $listMapper->addIdentifier('alleluiaVerse');
-        $listMapper->addIdentifier('summary');
-
+        $listMapper->addIdentifier(
+            'date',
+            null,
+            ['label' => 'liturgy.label.date']
+        );
+        $listMapper->addIdentifier(
+            'liturgyDay',
+            null,
+            ['label' => 'liturgy.label.liturgy_day']
+        );
+        $listMapper->addIdentifier(
+            'description',
+            null,
+            ['label' => 'liturgy.label.description']
+        );
+        $listMapper->add('color', null, ['label' => 'liturgy.label.color']);
+        $listMapper->add(
+            'isSolemnity',
+            null,
+            ['label' => 'liturgy.label.is_solemnity']);
+        $listMapper->add(
+            'isSolemnityVFC', null, ['label' => 'liturgy.label.is_solemnity_vfc']);
+        $listMapper->add('isCelebration', null, ['label' => 'liturgy.label.is_celebration']);
+        $listMapper->add('isCelebrationVFC', null, ['label' => 'liturgy.label.is_celebration_vfc']);
+        $listMapper->add(
+            'isMemorial',
+            null,
+            ['label' => 'liturgy.label.is_memorial']);
+        $listMapper->add(
+            'isMemorialVFC',
+            null,
+            ['label' => 'liturgy.label.is_memorial_vfc']
+        );
+        $listMapper->add(
+            'isMemorialFree',
+            null,
+            ['label' => 'liturgy.label.is_memorial_free']
+        );
+        $listMapper->add(
+            'yearType',
+            null,
+            ['label' => 'liturgy.label.year_type']
+        );
+        $listMapper->add('alleluiaReference', null, ['label' => 'liturgy.label.alleluia_reference']);
+        $listMapper->add('alleluiaVerse', null, ['label' => 'liturgy.label.allelulia_verse']);
+        $listMapper->addIdentifier('summary', null, ['label' => 'liturgy.label.summary']);
     }
 
-     protected function configureShowFields(ShowMapper $showMapper)
+    protected function configureShowFields(ShowMapper $showMapper)
     {
         $showMapper
                 ->with('General', ['class' => 'col-md-7'])->end()
                 ->with('Special', ['class' => 'col-md-5'])->end()
-                ->with('Text',['class' => 'col-md-12'])->end()
+                ->with('Text', ['class' => 'col-md-12'])->end()
         ;
 
         $showMapper
-            ->with('General')
-                ->add('date')
-                ->add('liturgyDay')
-                ->add('description')
-                ->add('color')
-                ->add('yearType')
+            ->with('General', ['label' => 'liturgy.title.general'])
+            ->add('summary', null, ['label' => 'liturgy.label.summary'])
+            ->add('date', null, ['label' => 'liturgy.label.date'])
+            ->add('liturgyDay', null, ['label' => 'liturgy.label.liturgy_day'])
+            ->add('description', null, ['label' => 'liturgy.label.description'])
+            ->add('color', null, ['label' => 'liturgy.label.color'])
+            ->add('yearType', null, ['label' => 'liturgy.label.year_type'])
             ->end()
-            ->with('Special')
-                ->add('isSolemnity')
-                ->add('isSolemnityVFC')
-                ->add('isCelebration')
-                ->add('isCelebrationVFC')
-                ->add('isMemorial')
-                ->add('isMemorialVFC')
-                ->add('isMemorialFree')
+            ->with('Special', ['label' => 'liturgy.title.special'])
+            ->add('isSolemnity', null, ['label' => 'liturgy.label.is_solemnity'])
+            ->add('isSolemnityVFC', null, ['label' => 'liturgy.label.is_solemnity_vfc'])
+            ->add('isCelebration', null, ['label' => 'liturgy.label.is_celebration'])
+            ->add('isCelebrationVFC', null, ['label' => 'liturgy.label.is_celebration_vfc'])
+            ->add('isMemorial', null, ['label' => 'liturgy.label.is_memorial'])
+            ->add('isMemorialVFC', null, ['label' => 'liturgy.label.is_memorial_vfc'])
+            ->add('isMemorialFree', null, ['label' => 'liturgy.label.is_memorial_free'])
             ->end()
-            ->with('Text')
-                ->add('alleluiaReference')
-                ->add('alleluiaVerse')
-                ->add('summary')
+            ->with('Text',  ['label' => 'liturgy.title.text'])
+            ->add('alleluiaReference', null, ['label' => 'liturgy.label.alleluia_reference'])
+            ->add('alleluiaVerse', null, ['label' => 'liturgy.label.alleluia_verse'])
+
             ->end()
             ;
     }
 
-    
+    public function toString($object)
+    {
+        $title = "";
+        if(!is_null($object->getDate()))
+        {
+            $title = $object->getDate()->format("Y-m-d");
+            $title = 'do '.$title;
+        }
+        return $object instanceof Liturgy
+            ? $object->getTitle()
+            : 'Liturgia '.$title; // shown in the breadcrumb on the create view
+    }
 }
